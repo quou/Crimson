49baa86f586dac68544ed7d937548931a3fa784f
George Lamb <georgeashleylamb@gmail.com>
2020-08-24 08:41:03 +1000

    Change SDL submodule to fork

--- Sandbox/Application.cpp
+++ Sandbox/Application.cpp
@@ -1,15 +1,10 @@
-#include <SDL.h>
-#include <glad/glad.h>
-
-#include <iostream>
-
 #include <Crimson.h>
 
 class Application : public Crimson::Application {
 private:
 public:
    void OnUpdate(float delta) override {
-      std::cout << "Hello" << '\n';
+
    }
 };
 
