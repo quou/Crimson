edd61b58c1edfd4240dcf99f99fd4e20c5414ba2
George Lamb <georgeashleylamb@gmail.com>
2020-08-23 15:59:35 +1000

    Glad & OpenGL

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -1,10 +1,17 @@
 #include <SDL.h>
+#include <glad/glad.h>
 
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
    SDL_Init(SDL_INIT_EVERYTHING);
-   SDL_Window* window = SDL_CreateWindow("Crimson", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+   SDL_Window* window = SDL_CreateWindow("Crimson", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
+   SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+   if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+      std::cout << "Error initialising Glad" << '\n';
+      return -1;
+   }
 
    bool running = true;
    while (running) {
@@ -14,6 +21,11 @@ int main(int argc, char const *argv[]) {
             running = false;
          }
       }
+
+      glClearColor(0.1f, 0.3f, 0.5f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      SDL_GL_SwapWindow(window);
    }
 
    SDL_DestroyWindow(window);
