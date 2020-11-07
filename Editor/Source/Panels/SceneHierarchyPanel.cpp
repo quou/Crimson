fd68df29e8feb2668e3c436c2ab39e910de74f25
George Lamb <georgeashleylamb@gmail.com>
2020-11-07 16:55:52 +1100

    Update editor look

--- Editor/Source/Panels/SceneHierarchyPanel.cpp
+++ Editor/Source/Panels/SceneHierarchyPanel.cpp
@@ -5,61 +5,54 @@
 
 #include <glm/gtc/type_ptr.hpp>
 
-static void DrawVec3Control(const std::string& label, glm::vec3& values, float resetValue = 0.0f, float colWidth = 100.0f) {
+static void DrawVec3Control(const std::string& label, glm::vec3& values, float resetValue = 0.0f, float columnWidth = 100.0f) {
 	ImGui::PushID(label.c_str());
 
 	ImGui::Columns(2, NULL, false);
-
-	ImGui::SetColumnWidth(0, colWidth);
+	ImGui::SetColumnWidth(0, columnWidth);
 	ImGui::Text("%s", label.c_str());
 	ImGui::NextColumn();
 
 	ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
-	ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+	ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
 
 	float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
-	ImVec2 buttonSize = {lineHeight + 3.0f, lineHeight};
+	ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
 
-	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.8f, 0.1f, 0.15f, 1.0f));
-	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.9f, 0.2f, 0.2f, 1.0f));
-	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.8f, 0.1f, 0.15f, 1.0f));
-	if (ImGui::Button("X", buttonSize)) {
+	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.9f, 0.2f, 0.2f, 1.0f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
+	if (ImGui::Button("X", buttonSize))
 		values.x = resetValue;
-	}
 	ImGui::PopStyleColor(3);
 
 	ImGui::SameLine();
-	ImGui::DragFloat("##X", &values.x, 0.1f);
+	ImGui::DragFloat("##X", &values.x, 0.1f, 0.0f, 0.0f, "%.2f");
 	ImGui::PopItemWidth();
 	ImGui::SameLine();
 
-	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.2f, 0.7f, 0.2f, 1.0f));
-	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.3f, 0.8f, 0.2f, 1.0f));
-	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.2f, 0.7f, 0.2f, 1.0f));
-	if (ImGui::Button("Y", buttonSize)) {
+	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.3f, 0.8f, 0.3f, 1.0f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
+	if (ImGui::Button("Y", buttonSize))
 		values.y = resetValue;
-	}
 	ImGui::PopStyleColor(3);
 
-
 	ImGui::SameLine();
-	ImGui::DragFloat("##Y", &values.y, 0.1f);
+	ImGui::DragFloat("##Y", &values.y, 0.1f, 0.0f, 0.0f, "%.2f");
 	ImGui::PopItemWidth();
 	ImGui::SameLine();
 
-	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.1f, 0.25f, 0.8f, 1.0f));
-	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.2f, 0.35f, 0.9f, 1.0f));
-	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.1f, 0.25f, 0.8f, 1.0f));
-	if (ImGui::Button("Z", buttonSize)) {
+	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.2f, 0.35f, 0.9f, 1.0f });
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
+	if (ImGui::Button("Z", buttonSize))
 		values.z = resetValue;
-	}
 	ImGui::PopStyleColor(3);
 
-
 	ImGui::SameLine();
-	ImGui::DragFloat("##Z", &values.z, 0.1f);
+	ImGui::DragFloat("##Z", &values.z, 0.1f, 0.0f, 0.0f, "%.2f");
 	ImGui::PopItemWidth();
-	ImGui::SameLine();
 
 	ImGui::PopStyleVar();
 
@@ -68,21 +61,65 @@ static void DrawVec3Control(const std::string& label, glm::vec3& values, float r
 	ImGui::PopID();
 }
 
-static void DrawTextControl(const std::string& label, std::string& string) {
+static void DrawTextControl(const std::string& label, std::string& string, float colWidth = 100.0f) {
 	ImGui::PushID(label.c_str());
 
 	ImGui::Columns(2, NULL, false);
 
-	ImGui::SetColumnWidth(0, 100.0f);
+
+	ImGui::SetColumnWidth(0, colWidth);
 	ImGui::Text("%s", label.c_str());
 	ImGui::NextColumn();
 
+	ImGui::PushMultiItemsWidths(1, ImGui::CalcItemWidth());
 	char buffer[256];
 	memset(buffer, 0, sizeof(buffer));
 	strcpy(buffer, string.c_str());
 	if (ImGui::InputText("##STRIN", buffer, sizeof(buffer))) {
 		string = std::string(buffer);
 	}
+
+	ImGui::PopItemWidth();
+
+	ImGui::Columns(1, NULL, false);
+
+	ImGui::PopID();
+}
+
+static void DrawFloatControl(const std::string& label, float* val, float step = 1.0f, float colWidth = 100.0f) {
+	ImGui::PushID(label.c_str());
+
+	ImGui::Columns(2, NULL, false);
+
+
+	ImGui::SetColumnWidth(0, colWidth);
+	ImGui::Text("%s", label.c_str());
+	ImGui::NextColumn();
+
+	ImGui::PushMultiItemsWidths(1, ImGui::CalcItemWidth());
+	ImGui::DragFloat("##FLIN", val, step);
+
+	ImGui::PopItemWidth();
+
+	ImGui::Columns(1, NULL, false);
+
+	ImGui::PopID();
+}
+
+static void DrawColorControl(const std::string& label, glm::vec3& value, float colWidth = 100.0f) {
+	ImGui::PushID(label.c_str());
+
+	ImGui::Columns(2, NULL, false);
+
+
+	ImGui::SetColumnWidth(0, colWidth);
+	ImGui::Text("%s", label.c_str());
+	ImGui::NextColumn();
+
+	ImGui::PushMultiItemsWidths(1, ImGui::CalcItemWidth());
+	ImGui::ColorEdit3("##COLIN", glm::value_ptr(value));
+
+	ImGui::PopItemWidth();
 
 	ImGui::Columns(1, NULL, false);
 
@@ -120,10 +157,29 @@ void SceneHierarchyPanel::DrawComponents(Crimson::Entity ent) {
 
 			auto& transform = ent.GetComponent<Crimson::TransformComponent>();
 
-			// Position
 			DrawVec3Control("Translation", transform.position);
 			DrawVec3Control("Rotation", transform.rotation);
 			DrawVec3Control("Scale", transform.scale, 1.0f);
+
+			ImGui::TreePop();
+		}
+	}
+
+	if (ent.HasComponent<Crimson::DirectionalLightComponent>()) {
+		if (ImGui::TreeNodeEx((void*)typeid(Crimson::DirectionalLightComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Directional Light")) {
+
+			DrawFloatControl("Intensity", &ent.GetComponent<Crimson::DirectionalLightComponent>().intensity, 0.01f);
+			DrawColorControl("Color", ent.GetComponent<Crimson::DirectionalLightComponent>().color);
+
+			ImGui::TreePop();
+		}
+	}
+
+	if (ent.HasComponent<Crimson::PointLightComponent>()) {
+		if (ImGui::TreeNodeEx((void*)typeid(Crimson::DirectionalLightComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Point Light")) {
+
+			DrawFloatControl("Intensity", &ent.GetComponent<Crimson::PointLightComponent>().intensity, 0.01f);
+			DrawColorControl("Color", ent.GetComponent<Crimson::PointLightComponent>().color);
 
 			ImGui::TreePop();
 		}
