0c4656049849ab7c45d33666a74278b7d2933e0d
George Lamb <georgeashleylamb@gmail.com>
2020-11-20 20:07:28 +1100

    Update shadows

--- Crimson/Source/Renderer/Renderer3D/LightScene.cpp
+++ Crimson/Source/Renderer/Renderer3D/LightScene.cpp
@@ -13,8 +13,8 @@ namespace Crimson {
 
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, 1024*m_maxDirectionalLights, 1024, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
 
-	   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 		float borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
