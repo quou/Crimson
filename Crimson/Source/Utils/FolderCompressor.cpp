d33ac690162e25d3a8905db8160d08e3738e098e
George Lamb <georgeashleylamb@gmail.com>
2020-11-26 16:21:14 +1100

    Update folder compressor

--- Crimson/Source/Utils/FolderCompressor.cpp
+++ Crimson/Source/Utils/FolderCompressor.cpp
@@ -19,11 +19,6 @@
 
 namespace Crimson {
 	void CompressFolder(const std::string& folder, const std::string& zipName) {
-		unsigned int numFiles = 0;
-		for (const auto& entry : std::filesystem::recursive_directory_iterator(folder)) {
-			numFiles++;
-		}
-
 		const char* comment = "No comment";
 
 		mz_zip_archive archive;
@@ -31,12 +26,7 @@ namespace Crimson {
 		mz_zip_writer_init_file(&archive, zipName.c_str(), 0);
 
 		g_currentCompressProgress.store(0.0f);
-		unsigned int fileProgress = 0;
-		g_isExporting = true;
 		for (const auto& entry : std::filesystem::recursive_directory_iterator(folder)) {
-			fileProgress++;
-			g_currentCompressProgress.store(1.0f / ((float)numFiles / (float)fileProgress));
-			CR_LOG("%f", g_currentCompressProgress.load());
 
 			if (!entry.is_directory()) {
 				mz_bool status = mz_zip_writer_add_file(&archive, entry.path().string().c_str(), entry.path().string().c_str(), comment, (uint16_t)strlen(comment), MZ_BEST_COMPRESSION);
--- Crimson/Source/Utils/FolderCompressor.h
+++ Crimson/Source/Utils/FolderCompressor.h
@@ -5,9 +5,6 @@
 #include <atomic>
 
 namespace Crimson {
-	static std::atomic<float> g_currentCompressProgress = 0.0f;
-	static std::atomic<bool> g_isExporting = false;
-
 	void CompressFolder(const std::string& folder, const std::string& zipName);
 	void CopyFile(const std::string& source, const std::string& destination);
 }
