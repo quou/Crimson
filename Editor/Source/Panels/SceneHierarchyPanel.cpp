dd2d11b6ed777a59b9a81699c73d971b10c1885d
George Lamb <georgeashleylamb@gmail.com>
2020-11-28 11:41:58 +1100

    Fix euler angle gimbal lock in editor

--- Editor/Source/Panels/SceneHierarchyPanel.cpp
+++ Editor/Source/Panels/SceneHierarchyPanel.cpp
@@ -182,12 +182,15 @@ void SceneHierarchyPanel::DrawComponents(Crimson::Entity ent) {
 		DrawTextControl("Tag", tag);
 
 		glm::vec3 newRotation = glm::degrees(glm::eulerAngles(component.rotation));
+		glm::vec3 oldRotation = glm::degrees(glm::eulerAngles(component.rotation));
 
 		DrawVec3Control("Translation", component.position);
 		DrawVec3Control("Rotation", newRotation);
 		DrawVec3Control("Scale", component.scale, 1.0f);
 
-		component.rotation = glm::radians(newRotation);
+		newRotation = oldRotation - newRotation;
+
+		component.rotation *= glm::quat(glm::radians(newRotation));
 	}, false);
 
 	DrawComponent<Crimson::DirectionalLightComponent>("Directional Light", ent, [](auto& component){
