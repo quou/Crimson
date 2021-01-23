b56b75b21c422f6f2d1026d878201e5d8e3d223c
George Lamb <georgeashleylamb@gmail.com>
2021-01-23 18:46:53 +1100

    Change default GUI column size

--- crimson/src/imgui/gui.h
+++ crimson/src/imgui/gui.h
@@ -6,11 +6,11 @@
 
 namespace Crimson {
 	bool DrawVec3Control(const std::string& label, vec3& values, float resetValue = 0.0f, float columnWidth = 80.0f);
-	void DrawTextControl(const std::string& label, std::string& string, float colWidth = 100.0f);
-	void DrawFloatControl(const std::string& label, float* val, float step = 1.0f, float min=0.0f, float max=0.0f, const std::string& tooltop = "", float colWidth = 100.0f);
-	void DrawIntControl(const std::string& label, int* val, int step=1, float colWidth = 100.0f);
-	void DrawBoolControl(const std::string& label, bool* val, float colWidth = 100.0f);
-	void DrawTextLabel(const std::string& label, const std::string& text, float colWidth = 100.0f);
-	void DrawColorControl(const std::string& label, vec3& value, float colWidth = 100.0f);
-	void DrawLinePlot(const std::string& label, float* values, unsigned int valueLength, float valuesOffset, float min, float max, float colWidth=100.0f);
+	void DrawTextControl(const std::string& label, std::string& string, float colWidth = 80.0f);
+	void DrawFloatControl(const std::string& label, float* val, float step = 1.0f, float min=0.0f, float max=0.0f, const std::string& tooltop = "", float colWidth = 80.0f);
+	void DrawIntControl(const std::string& label, int* val, int step=1, float colWidth = 80.0f);
+	void DrawBoolControl(const std::string& label, bool* val, float colWidth = 80.0f);
+	void DrawTextLabel(const std::string& label, const std::string& text, float colWidth = 80.0f);
+	void DrawColorControl(const std::string& label, vec3& value, float colWidth = 80.0f);
+	void DrawLinePlot(const std::string& label, float* values, unsigned int valueLength, float valuesOffset, float min, float max, float colWidth=80.0f);
 }
