9bc45ed153251320764d044274178d81e7e1155e
George Lamb <georgeashleylamb@gmail.com>
2021-01-23 18:17:04 +1100

    Add transform component to heirarchy

--- editor/src/heirarchy.cpp
+++ editor/src/heirarchy.cpp
@@ -1,4 +1,5 @@
 #include <imgui.h>
+#include <imgui_internal.h>
 
 #include "heirarchy.h"
 
@@ -21,6 +22,49 @@ namespace Crimson {
 		}
 	}
 
+
+	template <typename T, typename ComponentDrawFunction>
+	static void DrawComponent(const std::string& componentName, Entity* ent, ComponentDrawFunction drawFunction, bool canRemove = true) {
+		if (ent == NULL) { return; }
+
+		const ImGuiTreeNodeFlags treeNodeFlags = 
+		ImGuiTreeNodeFlags_DefaultOpen | ImGuiTreeNodeFlags_AllowItemOverlap | 
+		ImGuiTreeNodeFlags_Framed | ImGuiTreeNodeFlags_SpanAvailWidth | ImGuiTreeNodeFlags_FramePadding;
+
+		if (ent->HasComponent<T>()) {
+			ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(4, 4));
+
+			float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+			ImVec2 contentRegionAvail = ImGui::GetContentRegionAvail();
+
+			bool open = ImGui::TreeNodeEx((void*)typeid(T).hash_code(), treeNodeFlags, "%s", componentName.c_str());
+			ImGui::PopStyleVar();
+
+			ImGui::SameLine(contentRegionAvail.x - lineHeight * 0.5f);
+			if (ImGui::Button("+", ImVec2(lineHeight, lineHeight))) {
+				ImGui::OpenPopup("ComponentSettings");
+			}
+
+			bool removeComponent = false;
+			if (ImGui::BeginPopup("ComponentSettings")) {
+				if (ImGui::MenuItem("Remove Component", "", false, canRemove) && canRemove) {
+					removeComponent = true;
+				}
+				ImGui::EndPopup();
+			}
+
+			if (open) {
+				drawFunction(ent->GetComponent<T>());
+
+				ImGui::TreePop();
+			}
+
+			if (removeComponent) {
+				ent->RemoveComponent<T>();
+			}
+		}
+	}
+
 	void Heirarchy::OnDraw(const Camera& camera, const ref<Scene>& scene) {
 		ImGui::Begin("Heriarchy");
 
@@ -31,6 +75,23 @@ namespace Crimson {
 		if (ImGui::IsWindowHovered() && ImGui::IsMouseClicked(ImGuiMouseButton_Left)) {
 			m_selectionContext = NULL;
 		}
+
+		ImGui::End();
+
+		if (m_selectionContext != NULL) {
+			std::string propertiesTitle = m_selectionContext->m_name + " Properties";
+			ImGui::Begin((propertiesTitle + "###PROPERTIES").c_str());
+		} else {
+			ImGui::Begin("Properties###PROPERTIES");
+		}
+
+		DrawComponent<TransformComponent>("Transform", m_selectionContext, [](void* component){
+			TransformComponent* tc = (TransformComponent*)component;
+
+			DrawVec3Control("translation", tc->translation);
+			DrawVec3Control("rotation", tc->rotation);
+			DrawVec3Control("scale", tc->scale);
+		}, true);
 
 		ImGui::End();
 	}
