8e609f340d81ea4316327198948ad7248781f9ec
George Lamb <georgeashleylamb@gmail.com>
2021-02-09 17:20:57 +1100

    Update build scripts

--- build.sh
+++
@@ -1,3 +0,0 @@
-clear
-./premake5 gmake2
-make -j12 config=debug
--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-cd editor
-./../bin/editor
+clear
+./premake5 gmake2
+if make -j12 config=debug; then
+	echo === RESTART === 
+	
+	cd editor
+	./../bin/editor
+fi
